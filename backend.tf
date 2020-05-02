a60606a9f3d379c76c3880f04328ef5926575aa2
Taniusellu <tatianamoraru1998@gmail.com>
2020-05-02 20:43:18 +0000

    created

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+    required_version = "0.11.14"
+  backend "s3" {
+    bucket = "tatiana-state-bucket"
+    key    = "jenkins/us-east-1/tools/virginia/jenkins.tfstate"
+    region = "us-east-1"
+  }
+}
