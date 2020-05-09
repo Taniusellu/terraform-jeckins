c2d4ef2b13b49e99f5ecb79009e1c12277301910
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-05-08 21:16:19 -0500

    Create rt_ngw.tf

---
+++ module/rt_ngw.tf
@@ -0,0 +1,10 @@
+// RT Association
+resource "aws_route_table" "rt_ngw" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block =  "${var.nat_cidr_block}"
+    nat_gateway_id = "${aws_nat_gateway.ngw.id}"
+  }
+
+ tags = "${merge(var.tags, map("Name","rt_ngw"))}"
+}
