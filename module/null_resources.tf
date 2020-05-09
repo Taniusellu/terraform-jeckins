7bdb2430056720001adfee2ecd0915a8cb5156f9
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-05-08 21:23:45 -0500

    Update null_resources.tf

--- module/null_resources.tf
+++ module/null_resources.tf
@@ -12,10 +12,10 @@ resource "null_resource" "jenkins_password" {
     }
     
     inline = [
-      "sudo yum install java-1.8.0-openjdk-devel -y",
+      "sudo yum install java${var.java_version} -y",
       "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
       "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
-      "sudo yum install jenkins -y",
+      "sudo yum install ${var.jenkins_version} -y",
       "sudo systemctl start jenkins",
       "sudo systemctl enable jenkins",
       "echo -e $(tput setaf 1 )'Jenkins initialAdminPassword: '$(tput sgr0) $(tput setaf 2)`sudo cat /var/lib/jenkins/secrets/initialAdminPassword`$(tput sgr0)",  
